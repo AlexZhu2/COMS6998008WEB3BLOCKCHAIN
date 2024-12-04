1faea65e0c522e010439fe2881d4116ac97c2dcd
Kuai Yu <104323387+C1araYu@users.noreply.github.com>
2024-12-03 23:01:29 -0500

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -114,24 +114,22 @@ contract ArtystryXMarketplace is ERC721URIStorage {
     // Function to resell a token
     function resellToken(uint256 tokenId, uint256 price) public payable {
         require(ownerOf(tokenId) == msg.sender, "You must own the token to resell it");
-        require(msg.value >= listingPrice, "Insufficient listing fee");
+        require(msg.value == listingPrice, "Must pay exact listing price");
         require(price > 0, "Price must be greater than zero");
-        
-        // Refund excess ETH if overpaid
-        if (msg.value > listingPrice) {
-            payable(msg.sender).transfer(msg.value - listingPrice);
-        }
-    
+
+        // First approve the marketplace to handle the token
+        approve(address(this), tokenId);
+
         // Update the listed token details
         idToListedToken[tokenId].sold = false;
         idToListedToken[tokenId].price = price;
         idToListedToken[tokenId].seller = payable(msg.sender);
         idToListedToken[tokenId].owner = payable(address(this));
-    
+
         _itemsSold.decrement();
-    
+
         // Transfer the token to the contract for reselling
-        safeTransferFrom(msg.sender, address(this), tokenId);
+        _transfer(msg.sender, address(this), tokenId);
     }
 
 }
